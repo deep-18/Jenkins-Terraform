0ac21f1ae104f249ccb2241f1ee7c09c9308d069
Deep Raval <60513881+deep-18@users.noreply.github.com>
2024-03-03 12:39:57 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -19,7 +19,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
    ami = "ami-09988af04120b3591"
    instance_type = "t2.micro"
-   key_name = "Ansible"
+   key_name = "azureAgent"
    tags = {
       Name = "Example"
    }
